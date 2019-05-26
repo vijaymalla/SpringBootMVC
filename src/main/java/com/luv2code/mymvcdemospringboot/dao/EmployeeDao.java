6985bf1e24f49364af130683f132fb8636f5e518
vijaymalla <malla.vk1992@gmail.com>
2019-05-26 01:20:06 -0400

    Update EmployeeDao.java

--- src/main/java/com/luv2code/mymvcdemospringboot/dao/EmployeeDao.java
+++ src/main/java/com/luv2code/mymvcdemospringboot/dao/EmployeeDao.java
@@ -2,9 +2,22 @@ package com.luv2code.mymvcdemospringboot.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.luv2code.mymvcdemospringboot.model.Employee;
 
 public class EmployeeDao implements IEmployeeDao {
+	
+	private EntityManager entityManager;
+	
+	//Constructor Injection
+	@Autowired
+	public EmployeeDao(EntityManager entityManager) {
+		super();
+		this.entityManager = entityManager;
+	}
 
 	@Override
 	public List<Employee> getAllEmployees() {
